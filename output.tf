2498ac793654c76449502f1f172a7362eaadea83
Rachael Berecka <rachael.berecka@hashicorp.com>
2022-05-16 10:15:59 -0700

    add varset vars

--- output.tf
+++ output.tf
@@ -2,8 +2,11 @@ output "print-them-all" {
   value = <<EOT
     *** From root.auto.tfvars ***
     AutoVar: ${var.auto_number_var}
-    *** From variables.tf ***
+    *** values set in editor ***
     StringVar: ${var.string_var}
     NumberVar: ${var.number_var}
+    *** values set in varsets ***
+    WorkspaceVarsetVar: ${var.workspace_varset_var}
+    GlobalVarsetVar: ${var.global_varset_var}
   EOT
 }
